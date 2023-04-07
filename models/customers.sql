f8cb93cf16a2dd679f68605cd41db60fcbe2fbdc
Dave Berton <db@actuals.ca>
2023-04-07 07:33:01 -0400

    Update customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -24,7 +24,7 @@ customer_orders as (
         min(order_date) as first_order,
         max(order_date) as most_recent_order,
         count(order_id) as number_of_orders,
-        'foo5' as foo
+        'foo55' as foo
     from orders
 
     group by 1
